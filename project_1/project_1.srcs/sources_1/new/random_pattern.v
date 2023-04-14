a5bbbe88f54b3686c330ba19d7604cfc766694bc
wanggguidong1999 <wangguidong@mail.ustc.edu.cn>
2023-04-14 21:13:12 +0800

    some pattern

---
+++ project_1/project_1.srcs/sources_1/new/random_pattern.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/04/07 15:46:30
+// Design Name: 
+// Module Name: random_pattern
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module random_pattern(
+
+    );
+endmodule
