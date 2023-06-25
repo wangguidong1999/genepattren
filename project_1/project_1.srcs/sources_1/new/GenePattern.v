3100d68f9ae5f7a097715a9a4d0dfe21e191cfc0
Diana0525 <782272741@qq.com>
2023-06-25 18:43:55 +0800

    添加了PC元数据的读模块

--- project_1/project_1.srcs/sources_1/new/GenePattern.v
+++ project_1/project_1.srcs/sources_1/new/GenePattern.v
@@ -20,98 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module GenePattern(
-  input [63:0] current_trace_addr,
-  input [63:0] last_trace_addr,
-  input enable,
-  input clk,
-  input [63:0] recent_trace_value,
-  input [63:0] recent_last_trace_value,
-  input history_trace_is_stride,
-  input [63:0] offset,
-  input [63:0] last_stride,
-  input [63:0] last_trace_address2,
-  input reset,
-  input [63:0] data_in,
-  output [63:0] data_out,
-  output full,
-  output empty,
-  output reg pointer_or_not,
-  output reg [15:0] static_confidence_out,
-  output reg [15:0] indirect_confidence_out,
-  output reg [15:0] pointer_chase_confidence_out,
-  output reg [15:0] stride_confidence_out,
-  output reg [15:0] struct_pointer_confidence_out
-);
+module GenePattern#(parameter DATA_WIDTH=64)
+();
+    reg [DATA_WIDTH-1:0] current_trace_PC;
+    reg [DATA_WIDTH-1:0] current_trace_addr;
+    reg [DATA_WIDTH-1:0] current_trace_value;
+    reg clk;
+    reg enable;
+    reg PC_meta_data_reset;
+    reg indirect_pattern_reset;
+    //confidenceµÄ¶Á³ö
 
-//  // instantiate modules
-//  static_pattern static_pattern_inst(
-//    .current_trace_addr(current_trace_addr),
-//    .last_trace_addr(last_trace_addr),
-//    .enable(enable),
-//    .clk(clk),
-//    .static_confidence_in(16'd8),
-//    .static_confidence_out(static_confidence_out)
-//  );
 
-//  indirect_pattern indirect_pattern_inst(
-//    .current_trace_addr(current_trace_addr),
-//    .last_trace_addr(last_trace_addr),
-//    .recent_trace_value(recent_trace_value),
-//    .recent_last_trace_value(recent_last_trace_value),
-//    .history_trace_is_stride(history_trace_is_stride),
-//    .enable(enable),
-//    .clk(clk),
-//    .indirect_confidence_in(16'd8),
-//    .indirect_confidence_out(indirect_confidence_out)
-//  );
 
-//  pointer_array_pattern pointer_array_pattern_inst(
-//    .current_trace_addr(current_trace_addr),
-//    .recent_trace_value(recent_trace_value),
-//    .enable(enable),
-//    .clk(clk),
-//    .pointer_or_not(pointer_or_not)
-//  );
-
-//  pointer_chase_pattern pointer_chase_pattern_inst(
-//    .current_trace_value(current_trace_value),
-//    .current_trace_addr(current_trace_addr),
-//    .offset(offset),
-//    .enable(enable),
-//    .clk(clk),
-//    .pointer_chase_confidence_in(16'd8),
-//    .pointer_chase_confidence_out(pointer_chase_confidence_out)
-//  );
-
-//  stride_pattern stride_pattern_inst(
-//    .clk(clk),
-//    .enable(enable),
-//    .current_trace_address(current_trace_address),
-//    .last_stride(last_stride),
-//    .last_trace_address2(last_trace_address2),
-//    .stride(stride),
-//    .stride_confidence_in(16'd8),
-//    .stride_confidence_out(stride_confidence_out)
-//  );
-
-//  struct_pointer_pattern struct_pointer_pattern_inst(
-//    .current_trace_addr(current_trace_addr),
-//    .recent_trace_value(recent_trace_value),
-//    .enable(enable),
-//    .clk(clk),
-//    .struct_pointer_confidence_in(16'd8),
-//    .struct_pointer_confidence_out(struct_pointer_confidence_out)
-//  );
-
-//  offset_history_queue offset_history_queue_inst(
-//    .clk(clk),
-//    .reset(reset),
-//    .enq(enq),
-//    .deq(deq),
-//    .data_in(data_in),
-//    .data_out(data_out),
-//    .full(full),
-//    .empty(empty)
-//  );
 endmodule
--- project_1/project_1.srcs/sources_1/new/PC_meta_data.v
+++ project_1/project_1.srcs/sources_1/new/PC_meta_data.v
@@ -23,7 +23,9 @@
 module PC_meta_data#(parameter PC_SIZE=1024,parameter PC_WIDTH=$clog2(PC_SIZE),parameter WIDTH=64)
 (current_trace_addr,current_trace_value,current_trace_PC,pointer_array_confidence_in,pointer_array_finish,indirect_confidence_in,indirect_finish,
 pointer_chase_confidence_in,pointer_chase_finish,pointer_confidence_in,pointer_finish,regional_random_confidence_in,regional_random_finish,
-static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_pointer_confidence_in,struct_pointer_finish,clk,enable,reset,add_finish);
+static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_pointer_confidence_in,struct_pointer_finish,clk,enable,reset,read,write,write_finish,
+pointer_array_confidence_out,indirect_confidence_out,pointer_chase_confidence_out,pointer_confidence_out,regional_random_confidence_out,static_confidence_out,
+stride_confidence_out,struct_pointer_confidence_out,read_finish);
     input wire [WIDTH-1:0] current_trace_addr;
     input wire [WIDTH-1:0] current_trace_value;
     input wire [WIDTH-1:0] current_trace_PC;
@@ -46,11 +48,23 @@ static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_poi
     input wire clk;
     input wire enable;
     input wire reset;
-    output reg add_finish;
+    input wire read;
+    input wire write;
+    output reg write_finish;
+    output reg [15:0] pointer_array_confidence_out;
+    output reg [15:0] indirect_confidence_out;
+    output reg [15:0] pointer_chase_confidence_out;
+    output reg [15:0] pointer_confidence_out;
+    output reg [15:0] regional_random_confidence_out;
+    output reg [15:0] static_confidence_out;
+    output reg [15:0] stride_confidence_out;
+    output reg [15:0] struct_pointer_confidence_out;
+    output reg read_finish;
     reg [WIDTH-1:0] array [0:PC_SIZE-1][0:10];//PC||mem_addr||value||8*confidence一起存，同时存进recent里
     reg [15:0] hash;
     reg [15:0] index;
-    reg [1:0] state;
+    reg [2:0] write_state;
+    reg [1:0] read_state;
     reg [10:0] initial_count1;
     reg [3:0] initial_count2;
     always@(*)
@@ -61,14 +75,18 @@ static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_poi
     begin
         if (reset)
         begin
-            add_finish <= 0;
-            state <= 2'b00;
+            write_finish <= 0;
+            read_finish <= 0;
             initial_count1 <= 0;
             initial_count2 <= 0;
+        end
+        if(write)
+        begin
+            write_state <= 3'b000;
             index <= hash;
         end
-        case(state)
-       2'b00://初始化所有的array元素为0
+        case(write_state)
+        3'b000://初始化所有的array元素为0
         begin
             array[initial_count1][initial_count2] <= 0;
             initial_count2 <= initial_count2 + 1;
@@ -79,20 +97,33 @@ static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_poi
             end
             else if (initial_count2 == 11 && initial_count1 == PC_SIZE)
             begin
-                state <= 2'b01;
+                write_state <= 3'b001;
             end
         end
-        2'b01://找到PC应该存放的位置
+        3'b001://找到PC应该存放的位置
         begin
             if(array[index][0] == 0)//哈希有空位
             begin
                 array[index][0] <= current_trace_PC;
                 array[index][1] <= current_trace_addr;
                 array[index][2] <= current_trace_value;
-                state <= 2'b10;
+                write_state <= 3'b010;
+            end
+            else
+            begin
+                index <= index + 1;
+                if (index == PC_SIZE)
+                begin
+                    index <= 0;
+                end
+                else if (index == hash - 1)//没有合适的位置则考虑驱逐出现次数最少的PC的元数据
+                begin
+                    
+                end
+                
             end
         end
-        2'b10://接收confidence
+        3'b010://接收confidence
         begin
             if(pointer_array_finish)
             begin
@@ -129,13 +160,46 @@ static_confidence_in,static_finish,stride_confidence_in,stride_finish,struct_poi
             if (pointer_array_finish && indirect_finish && pointer_chase_finish && pointer_finish && 
             regional_random_finish && static_finish && stride_finish && struct_pointer_finish)
             begin
-                state <= 2'b11;
+                write_state <= 3'b011;
             end
         end
-        2'b11:
+        3'b011:
+        begin
+            write_finish <= 1;
+        end
+        3'b100://驱逐出现次数最少的PC占据元数据的位置
+        begin
+            
+        end
+        endcase
+        if(read)
+        begin
+            read_state <= 2'b00;
+            index <= hash;
+        end
+        case(read_state)
+        2'b00:
+        begin
+            if (array[index][0] == current_trace_PC)
+            begin
+                pointer_array_confidence_out <= array[index][3];
+                indirect_confidence_out <= array[index][4];
+                pointer_chase_confidence_out <= array[index][5];
+                pointer_confidence_out <= array[index][6];
+                regional_random_confidence_out<= array[index][7];
+                static_confidence_out <= array[index][8];
+                stride_confidence_out <= array[index][9];
+                struct_pointer_confidence_out <= array[index][10];
+            end
+            else
+            begin
+                read_state <= 2'b01;
+            end
+        end
+        2'b01:
         begin
             index <= index + 1;
-            add_finish <= 1;
+            
         end
         endcase
     end
